016f59a46347927b1f2da4b7943ada55bbec262e
root <nicolasjonason@gmail.com>
2025-03-18 14:47:24 +0000

    add stuff

--- pianophi_trn.py
+++ pianophi_trn.py
@@ -1,12 +1,13 @@
 #%%
+import miditok
 import symusic
 import transformers
-from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 from trl import GRPOConfig, GRPOTrainer
 from datasets import Dataset, load_dataset
 from symusic import Synthesizer, BuiltInSF3, dump_wav
 from audiobox_aesthetics.infer import initialize_predictor
+
 
 sample_rate = 44100
 sf_path = BuiltInSF3.MuseScoreGeneral().path(download=True)
@@ -16,51 +17,38 @@ synth = Synthesizer(
     sample_rate = sample_rate, # the sample rate of the output wave, sample_rate is the default value
 )
 #%%
-checkpoint = "session-gpt/checkpoints/nospace/checkpoint-8000"
-tokenizer = AutoTokenizer.from_pretrained("distilgpt2")
-tokenizer.add_special_tokens({"pad_token": "[PAD]"})
-model = AutoModelForCausalLM.from_pretrained(checkpoint)
+model = transformers.AutoModelForCausalLM.from_pretrained("lucacasini/metamidipianophi3", trust_remote_code=True, torch_dtype="auto")
+tokenizer = miditok.REMI.from_pretrained("lucacasini/metamidipianophi3")
 
-OUTPUT_DIR = "artefacts/session-space-structure"
+
+OUTPUT_DIR = "artefacts/pianophi-kl=0.1"
 #%%
 def gen():
-    yield {"prompt": "This "}
+    yield {"prompt": ""}
 ds = Dataset.from_generator(gen)
+print(ds[0])
 
+
+def reward_len(completions, **kwargs):
+    sms = [tokenizer(completions[i].cpu().numpy()[None,...] ) for i in range(completions.shape[0])]
+    rewards = [sm.note_num() for sm in sms]
+    print(f"average reward: {sum(rewards)/len(rewards)}")
+    print(f"Rewards: {rewards}")
+    return rewards
 
 aes_predictor = initialize_predictor()
 
 SAVE_INTERVAL = 100
 reward_step = 0
 
-
-
-def extract_abc(abc):
-    if "@" not in abc:
-        return "invalid"
-    if "€" not in abc:
-        return "invalid"
-    abc = abc.split("@")[1]
-    abc = abc.split("€")[0]
-    return abc    
-
 def aes_reward(completions, **kwargs):
-    abc = [extract_abc(c) for c in completions]
-    sms = []
-    for a in abc:
-        try:
-            sms.append(symusic.Score.from_abc(a))
-        except:
-            print(f"Error parsing abc: {a}")
-            sms.append(symusic.Score())
-    audio = []
-    for sm in sms:
-        try:
-            audio.append(synth.render(sm))
-        except:
-            print(f"Error rendering score: {sm}")
-            audio.append(torch.zeros
-            (1,44100*1))
+    print(f"Decoding to MIDI")
+    sms = [tokenizer(completions[i].cpu().numpy()[None,...] ) for i in range(completions.shape[0])]
+
+
+
+    print(f"Rendering to audio")
+    audio = [synth.render(sm) for sm in sms]
     predictor_inputs = [{"path": torch.tensor(audio[i]).float(), "sample_rate": 44100} for i in range(len(audio))]
     print(f"Predicting aesthetics")
     scores = aes_predictor.forward(predictor_inputs)
@@ -69,6 +57,7 @@ def aes_reward(completions, **kwargs):
     rewards = [sum([score["CE"], score["CU"], score["PC"], score["PQ"]]) for score in scores]
     # rewards = [score["CE"] for score in scores]
 
+    # rewards = [float(sm.note_num()) for sm in sms]
     print(f"average reward: {sum(rewards)/len(rewards)}")
 
     print(f"Rewards: {rewards}")
@@ -83,6 +72,40 @@ def aes_reward(completions, **kwargs):
     reward_step += 1
     return rewards
 
+#%%
+#%%
+
+
+
+
+class DummyTokenizer():
+    def __init__(self,tokenizer):
+        self.pad_token_id = tokenizer.vocab["PAD_None"]
+        self.eos_token_id = tokenizer.vocab["EOS_None"]
+        self.bos_token_id = tokenizer.vocab["BOS_None"]
+
+    def encode(self, x, **kwargs):
+        # print(f"Called encode with {x}")
+        return {"input_ids": torch.tensor([[1]])}
+    def decode(self, x, **kwargs):
+        # print(f"Called decode with {x}")
+        return x
+    def batch_decode(self, x, **kwargs):
+        # print(f"Called batch_decode with {x}")
+        return x
+    
+    def save_pretrained(self, path):
+        print(f"Calling save_pretrained with {path} (does nothing)")
+
+    def __call__(self, x, **kwargs):
+        # print(f"Called __call__ with {x}")
+        n_samples = len(x)
+        return {
+                "input_ids": torch.tensor([[1] for sample in range(n_samples)]), 
+                "attention_mask":torch.tensor([[1] for sample in range(n_samples)]),
+            }
+    
+dummy_tokenizer = DummyTokenizer(tokenizer)
 # %%
 
 #%%
@@ -95,8 +118,8 @@ BATCH_SIZE=16
 config = GRPOConfig(
     temperature=1.0,
     output_dir=OUTPUT_DIR,
-    max_completion_length=200,
-    # max_prompt_length=1,
+    max_completion_length=250,
+    max_prompt_length=1,
     num_train_epochs=100_000,
     learning_rate=1e-5,
     report_to="wandb",
@@ -104,14 +127,14 @@ config = GRPOConfig(
     num_generations=BATCH_SIZE,
     per_device_train_batch_size=BATCH_SIZE,
     save_steps=5000,
-    beta=0.5,
+    beta=0.1
 )
 trainer = GRPOTrainer(
     model=model,
     reward_funcs=aes_reward,
     args =  config,
     train_dataset=ds,
-    processing_class=tokenizer,
+    processing_class=dummy_tokenizer,
 
 )
 # save model
@@ -119,11 +142,3 @@ trainer.save_model()
 trainer.train()
 #%%
 #%%
-import symusic
-score = "artefacts/session/fs_renders/16900/reward=25.907721996307373_7.mid"
-
-# check time signature
-sm = symusic.Score(score)
-
-print(sm.time_signatures)
-# %%
